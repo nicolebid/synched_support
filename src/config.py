0256f72e246caa9737bd0f9141305141c9b4a0e8
nicolebid <nrbidwell@gmail.com>
2025-02-21 23:54:40 -0800

    adjusted csv structure;

--- src/config.py
+++ src/config.py
@@ -6,6 +6,6 @@ DATA_DIR = os.path.join(ROOT_DIR, 'data')
 
 # file paths 
 STUDENT_DATA = os.path.join(DATA_DIR, 'student.csv')
-ATTEND_CLASS_DATA = os.path.join(DATA_DIR, 'attendance_class_records.csv')
-ATTEND_SUPPORT_DATA = os.path.join(DATA_DIR, 'attendance_support_record.csv')
+ATTEND_DATA = os.path.join(DATA_DIR, 'attendance_class_data_edit.csv')
+# ATTEND_SUPPORT_DATA = os.path.join(DATA_DIR, 'attendance_class_data_edit.csv')
 DEADLINES_DATA = os.path.join(DATA_DIR, 'deadlines.csv')
